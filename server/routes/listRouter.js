0d552068c9068e6adf28a2c56c53a4e3ad42ce00
Selbin_C_R <selbinzr@gmail.com>
2020-01-16 11:54:55 +0530

    created list and todo routers

--- server/routes/listRouter.js
+++ server/routes/listRouter.js
@@ -1,3 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const listHandler = require('../controller/listHandler')
 
+// shows all list
+router.get('/', listHandler.showAllList)
+
+// creates a new list
+router.post('/', listHandler.createList)
+
+// updates a existing list
+router.put('/:list_id', listHandler.updateList)
+
+// delete a list
+router.delete('/:list_id', listHandler.deleteList)
---
+++ server/routes/todoRouter.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const todoHandler = require('../controller/todoHandler')
+
+// shows all todo
+router.get('/list_id', todoHandler.showAllTodo)
+
+// creates a new todo
+router.post('/list_id', todoHandler.createTodo)
+
+// updates a existing list
+router.put('/:todo_id', todoHandler.updateTodo)
+
+// delete a list
+router.delete('/:todo_id', todoHandler.deleteTodo)
