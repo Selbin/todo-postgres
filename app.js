f4d227ddf2c6e4d364498f7e9de77ed7a710d4d2
Selbin_C_R <selbinzr@gmail.com>
2020-01-16 11:55:51 +0530

    handling routing

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
-
+const listRoutes = require('./server/routes/listRouter')
+const todoRoutes = require('./server/routes/todoRouter')
+const dotEnv = require('dotenv')
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => res.send('hi'))
-app.listen(port, () => console.log(`listening to port ${port}`))
+dotEnv.config()
+
+app.use(express.json())
+app.use('/list', listRoutes)
+app.use('/todo', todoRoutes)
+
+app.listen(process.env.APP_PORT, () => console.log(`listening to port ${process.env.APP_PORT}`))
